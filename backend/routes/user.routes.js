5a661359bb1ee9d3b69d7d37df5fd5b1e1439591
Md Selim <whiteblueskyss@gmail.com>
2025-03-04 03:36:57 +0600

    Added user routes and controller

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { protectRoute } from '../middlewares/protectRoute.js';
+import { followUnfollowUser, getSuggestedUsers, getUserProfile, updateUser } from '../controllers/user.controller.js';
+
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
+router.get("/suggested", protectRoute, getSuggestedUsers);
+router.post('/update', protectRoute, updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from "express";
-import authRoutes from "./routes/auth.routes.js";
 import dotenv from "dotenv";
-import connectMongodDB from "./db/connectMongodDB.js";
 import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
+
+import connectMongodDB from "./db/connectMongodDB.js";
+import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json()); // to parse incoming requests with JSON payloads
@@ -13,6 +21,7 @@ app.use(express.urlencoded({extended: true})); // to parse incoming requests wit
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 
 const PORT = process.env.PORT || 8000;
