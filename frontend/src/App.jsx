c8026583d83375b21579f4a797940305fa3e36e8
Md Selim <whiteblueskyss@gmail.com>
2025-03-12 14:07:01 +0600

    Project Updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
-import { Route, Routes } from "react-router-dom";
-
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/home/HomePage';
+import SignUpPage from './pages/auth/signup/SignUpPage';
+import LoginPage from './pages/auth/login/LoginPage';
+import Sidebar from './components/common/Sidebar';
+import RightPanel from './components/common/RightPanel';
+import Notification from './pages/notification/NotificationPage';
+import ProfilePage from './pages/profile/ProfilePage';
 
 function App() {
 	return (
 		<div className='flex max-w-6xl mx-auto'>
+
+			<Sidebar />
 			<Routes>
-				
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/notifications' element={<Notification />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
 			</Routes>
+			<RightPanel />
 		</div>
 	);
 }
