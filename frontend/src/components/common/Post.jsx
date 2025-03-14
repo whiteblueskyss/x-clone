c6575edfa6fb6f9a623b8a819b273aef8d46032f
Md Selim <whiteblueskyss@gmail.com>
2025-03-14 12:01:31 +0600

    delete post functionality added

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -5,19 +5,49 @@ import { FaRegBookmark } from "react-icons/fa6";
 import { FaTrash } from "react-icons/fa";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
+
+	const {data: authUser} = useQuery({queryKey: ["authUser"]});
+
+	const {mutate: deletePost, isPending} = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/delete/${post._id}`, {
+					method: "DELETE",
+				})
+
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Failed to delete post");	
+				return data;
+			}
+			catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Post deleted successfully");
+		},
+	
+	});
+
+
 	const postOwner = post.user;
 	const isLiked = false;
 
-	const isMyPost = true;
+	const isMyPost = authUser._id === post.user._id;
 
 	const formattedDate = "1h";
 
 	const isCommenting = false;
 
-	const handleDeletePost = () => {};
+	const handleDeletePost = () => {
+		deletePost();
+	};
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
@@ -45,7 +75,11 @@ const Post = ({ post }) => {
 						</span>
 						{isMyPost && (
 							<span className='flex justify-end flex-1'>
-								<FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+								{!isPending && ( < FaTrash className='cursor-pointer hover:text-red-500' onClick={handleDeletePost} />
+								)}
+								{isPending && (
+									<LoadingSpinner className='w-4 h-4 text-gray-500 animate-spin' />
+								)}
 							</span>
 						)}
 					</div>
@@ -78,7 +112,7 @@ const Post = ({ post }) => {
 										{post.comments.length === 0 && (
 											<p className='text-sm text-slate-500'>
 												No comments yet 🤔 Be the first one 😉
-											</p>
+											</p>	
 										)}
 										{post.comments.map((comment) => (
 											<div key={comment._id} className='flex gap-2 items-start'>
